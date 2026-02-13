66ff383d67d6a54785d93675d799c2637d762011
Nikita <miracleqxz@gmail.com>
2026-02-13 17:43:21 +0300

    tasks/bootstrap-0: initialize Spring Boot 3.5.10 project skeleton

---
+++ src/main/java/com/bntu/chinesecourses/ChinesecoursesAdminApplication.java
@@ -0,0 +1,12 @@
+package com.bntu.chinesecourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinesecoursesAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChinesecoursesAdminApplication.class, args);
+  }
+}
---
+++ src/test/java/com/bntu/chinesecoursesadmin/ChinesecoursesAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bntu.chinesecoursesadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinesecoursesAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
